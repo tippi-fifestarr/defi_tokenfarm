7f7aad4d496b87108771012676c67fef0fe7b302
tippi-fifestarr <62179036+tippi-fifestarr@users.noreply.github.com>
2021-06-17 10:08:51 -0500

    Updates

---
+++ precache-manifest.3cb2317b8b35e896d7b8ad628a65367e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "e7c14de11dc1ba0d2067",
+    "url": "/defi_tokenfarm/static/css/main.ade1dd74.chunk.css"
+  },
+  {
+    "revision": "e7c14de11dc1ba0d2067",
+    "url": "/defi_tokenfarm/static/js/main.e7c14de1.chunk.js"
+  },
+  {
+    "revision": "0bfafcb3d8f690d832ef",
+    "url": "/defi_tokenfarm/static/css/1.3595a05e.chunk.css"
+  },
+  {
+    "revision": "0bfafcb3d8f690d832ef",
+    "url": "/defi_tokenfarm/static/js/1.0bfafcb3.chunk.js"
+  },
+  {
+    "revision": "bd24400052315b5974d9",
+    "url": "/defi_tokenfarm/static/js/runtime~main.bd244000.js"
+  },
+  {
+    "revision": "484c025d2a5ce4d19dbaaea94d88f8d8",
+    "url": "/defi_tokenfarm/static/media/go_d.484c025d.png"
+  },
+  {
+    "revision": "29984ac6f20bee7ed8ce502bedb90415",
+    "url": "/defi_tokenfarm/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/defi_tokenfarm/precache-manifest.3cb2317b8b35e896d7b8ad628a65367e.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/defi_tokenfarm/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+});
